04d66178f6b75562cc91ec62f397427ec909c2ee
Sweta Dahal <dahalsweta5@gmail.com>
2026-07-01 11:47:59 +0545

    Feat: Connect Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,46 @@
-function Home () {
-    return <h1>Home Page</h1>
+import { useProducts } from "../hooks/useProducts";
+
+function Home() {
+  const {
+    data,
+    isLoading,
+    error,
+  } = useProducts();
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Something went wrong</h1>;
+  }
+
+  return (
+    <div className="p-6">
+
+      <h1 className="text-3xl font-bold mb-6">
+        Products
+      </h1>
+
+      <div className="space-y-4">
+        {data?.map((product) => (
+          <div
+            key={product.id}
+            className="border p-4 rounded"
+          >
+            <h2>
+              {product.title}
+            </h2>
+
+            <p>
+              ${product.price}
+            </p>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
 }
 
 export default Home;
