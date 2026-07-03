8242ea4e13e9486145c923ce10704bd35e92be4e
Sweta Dahal <dahalsweta5@gmail.com>
2026-07-03 09:01:33 +0545

    Feat:Create login function

---
+++ src/api/authApi.ts
@@ -0,0 +1,15 @@
+import { api } from "./axios";
+import type {
+  LoginRequest,
+  LoginResponse,
+} from "../types/Auth";
+
+export async function loginUser(
+    credentials: LoginRequest
+): Promise<LoginResponse> {
+    const response = await api.post(
+        "/auth/login",
+        credentials
+    );
+    return response.data;
+}
