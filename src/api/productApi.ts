a583fea50889bcd6debb3f9bd231ad573f76d354
Sweta Dahal <dahalsweta5@gmail.com>
2026-07-06 12:54:43 +0545

    Feat: URL mapping for searching products

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -1,5 +1,5 @@
 import { api } from "./axios";
-import type { Product } from "../types/product";
+import type { Product } from "../types/Product";
 
 export async function fetchProducts(): Promise<Product[]> {
   const response = await api.get("/products");
