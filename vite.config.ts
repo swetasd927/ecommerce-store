2fb11d65148983ee666c21deffdafacd469d0250
Sweta Dahal <dahalsweta5@gmail.com>
2026-07-07 12:47:07 +0545

    Fix: proxy issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,37 @@
+// import { defineConfig } from 'vite'
+// import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+// import babel from '@rolldown/plugin-babel'
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     babel({ presets: [reactCompilerPreset()] }),
+//      tailwindcss(),
+//   ],
+// })
+
+
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-     tailwindcss(),
+    tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fakestoreapi.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
