8409b661e7a89891625b986b4209a66748b480e0
Sweta Dahal <dahalsweta5@gmail.com>
2026-07-09 15:06:40 +0545

    chore: footer enhance

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -35,13 +35,7 @@ function Footer() {
     <footer className="footer-wrap">
       <div className="mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
         <div className="grid grid-cols-2 gap-8 sm:grid-cols-4">
-          {/* Logo */}
-          <div className="col-span-2 sm:col-span-1">
-            <span className="text-ink-primary font-display text-lg font-bold">
-              LOGO
-            </span>
-          </div>
-
+        
           {/* Link columns */}
           {footerLinks.map((section) => (
             <div key={section.heading}>
@@ -80,7 +74,7 @@ function Footer() {
 
         {/* Bottom bar */}
         <div className="footer-bottom mt-8 pt-6 text-center">
-          © 2026 ECOM. All rights reserved.
+          © 2026 E-SHOP. All rights reserved.
         </div>
       </div>
     </footer>
