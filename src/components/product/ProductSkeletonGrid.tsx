bb535e4853b3ccfe56d550f9d83cec31ab561e01
Sweta Dahal <dahalsweta5@gmail.com>
2026-07-10 15:33:09 +0545

    Fix: Product skeleton loading

--- src/components/product/ProductSkeletonGrid.tsx
+++ src/components/product/ProductSkeletonGrid.tsx
@@ -2,8 +2,8 @@ import ProductSkeleton from "../ui/ProductSkeleton";
 
 function ProductSkeletonGrid() {
   return (
-    <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 lg:gap-5">
-      {Array.from({ length: 8 }).map((_, index) => (
+    <div className="grid grid-cols-2 items-stretch gap-3 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 lg:gap-4">
+      {Array.from({ length: 10 }).map((_, index) => (
         <ProductSkeleton key={index} />
       ))}
     </div>
@@ -12,25 +12,3 @@ function ProductSkeletonGrid() {
 
 export default ProductSkeletonGrid;
 
-
-// //for full
-// //product detail page
-
-
-// import ProductSkeleton from "../ui/ProductSkeleton";
-
-// type ProductSkeletonGridProps = {
-//   count?: number;
-// };
-
-// function ProductSkeletonGrid({ count = 8 }: ProductSkeletonGridProps) {
-//   return (
-//     <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 lg:gap-4">
-//       {Array.from({ length: count }).map((_, index) => (
-//         <ProductSkeleton key={index} />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default ProductSkeletonGrid;
